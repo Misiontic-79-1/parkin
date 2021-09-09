240e08609983a509c889fbf92d5b824d2f459868
CAAMCODE <85082707+CAAMCODE@users.noreply.github.com>
2021-09-08 22:01:32 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,29 @@
+
+public class HolaMundo {
+    
+    public static void main(String arg[]){
+        System.out.println("Hola Mundo Java ");
+        
+        int miVariableEntera = 12;
+        System.out.println(miVariableEntera);
+        
+        miVariableEntera= 5;
+        //modificamos el valor de la variable
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena ="Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena= "Adios";
+        
+        System.out.println(miVariableCadena);
+        //var- inferencia de tipo en java
+        var miVarEn= 10;
+        System.out.println(miVarEn);
+        
+                
+                 
+                
+    }
+    
+}
